9e86db88a764ffbfbfdf8e9fc1c4dc102022eae0
Andrea Walsh <andreawalsh@Andreas-Air.fios-router.home>
2020-03-09 20:49:47 -0400

    cleaned up tests

--- tests/strict-equality.test.js
+++ tests/strict-equality.test.js
@@ -1,66 +1,54 @@
 const describe = require('mocha').describe
 const it = require('mocha').it
 const expect = require('chai').expect
-const strictequal = require('../strict-equality')
+const strictEqual = require('../strict-equality')
 
 
 describe('strict-equality', () => {
   describe('compare', () => {
-    it('determines inputone and inputtwo as equal numbers', () => {
+    it('determines 2 and 2 are equal numbers', () => {
       //create test data
       const inputOne = 2
       const inputTwo = 2
 
       //call function with test data
-      const equality = strictequal(inputOne, inputTwo)
+      const equality = strictEqual(inputOne, inputTwo)
 
       //assertions about what happens
       expect(equality).to.equal(true)
     })
 
-    it('determines inputone and inputtwo as equal strings', () => {
+    it('determines "a" and "a" are equal strings', () => {
       //create test data
       const inputOne = 'a'
       const inputTwo = 'a'
 
       //call function with test data
-      const equality = strictequal(inputOne, inputTwo)
+      const equality = strictEqual(inputOne, inputTwo)
 
       //assertions about what happens
       expect(equality).to.equal(true)
     })
 
-    it('determines inputone and inputtwo as unequal numbers', () => {
+    it('determines 2 and 4 are not equal', () => {
       //create test data
       const inputOne = 2
       const inputTwo = 4
 
       //call function with test data
-      const equality = strictequal(inputOne, inputTwo)
+      const equality = strictEqual(inputOne, inputTwo)
 
       //assertions about what happens
       expect(equality).to.equal(false)
     })
 
-    it('determines that inputone and inputtwo as unequal', () => {
+    it('determines that 2 and "2" are not equal', () => {
       //create test data
       const inputOne = 2
-      const inputTwo = 'two'
+      const inputTwo = '2'
 
       //call function with test data
-      const equality = strictequal(inputOne, inputTwo)
-
-      //assertions about what happens
-      expect(equality).to.equal(false)
-    })
-
-    it('determines inputone andinputtwo as uneuqal because of input type', () => {
-      //create test data
-      const inputOne = 2
-      const inputTwo = 'a'
-
-      //call function with test data
-      const equality = strictequal(inputOne, inputTwo)
+      const equality = strictEqual(inputOne, inputTwo)
 
       //assertions about what happens
       expect(equality).to.equal(false)
